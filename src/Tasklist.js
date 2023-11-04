6c9bc30537510883624e76b3be9de82b75a10651
Eetu Haverinen <eetu.haverinen@metropolia.fi>
2023-11-04 12:20:05 +0200

    järjestys deadlinen mukaan

--- src/Tasklist.js
+++ src/Tasklist.js
@@ -3,9 +3,11 @@ import Task from './Task';
 import './TaskList.css';
 
 function TaskList({ tasks, onTaskClick, onEdit, onDelete }) {
+    const sortedTasks = tasks.slice().sort((a, b) => new Date(a.deadline) - new Date(b.deadline));
+
     return (
         <div className="task-list">
-            {tasks.map(task => (
+            {sortedTasks.map(task => (
                 <Task key={task._id} task={task} onTaskClick={onTaskClick} onEdit={onEdit} onDelete={onDelete} />
             ))}
         </div>
